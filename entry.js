11b4547d34ba117eb0ff8d7dfcf4b2b39c819274
Michael Tiller <michael.tiller@gmail.com>
2015-09-24 14:48:39 +0200

    Got rid of App.* files

--- entry.js
+++ entry.js
@@ -1,6 +1,4 @@
-require("./js/App.js");
 var Impact = require("./js/Impact.js");
 
 // This kicks the application off.
-console.log("Impact = ", Impact);
 Impact.Application.Mount(document.getElementById('impact'));
--- src/App.ts
+++
@@ -1,5 +0,0 @@
-import Impact = require("./Impact");
-
-export function Mount(node: Element) {
-	Impact.Application.Mount(node);
-}
