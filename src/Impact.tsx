833c085c1dfbcd3c18982fa9d8d3e906c0e1a612
Michael Tiller <michael.tiller@gmail.com>
2015-09-26 10:35:46 +0200

    Playing around with ideas for observables and mutables in the context of a global state

--- src/Impact.tsx
+++ src/Impact.tsx
@@ -21,8 +21,55 @@ class RootState {
 	constructor() { }
 }
 
+// This is the read-only part of the interface
+export interface Observable<S> {
+	get(): S;
+}
+
+export interface Mutable<S> {
+    update(v: S): void;
+}
+
+export class SubState<S> implements Observable<S>, Mutable<S> {
+	private subscribers: Array<(nv: S, ov: S) => void>;
+	constructor(private value: S) {
+		this.subscribers = [];
+	}
+	update(v: S) {
+		var old = this.value;
+		this.value = v;
+		this.subscribers.forEach((f) => { f(v, old) })
+	}
+	get(): S { return this.value; }
+	notify(f: (nv: S, ov: S) => void) {
+		this.subscribers.push(f);
+	}
+}
+
+export class Store {
+	private index: SubState<Index.ImpactIndex>;
+
+	constructor() {
+		this.index = new SubState(null);
+	}
+
+	getIndex(): Observable<Index.ImpactIndex> {
+		return this.index;
+	}
+
+	load() {
+		var source = "http://impact.github.io/impact_index.json";
+		$.get(source, (result) => {
+			this.index = result;
+		})
+	}
+}
+
 export class Application extends React.Component<{}, RootState> {
 	static Mount(node: Element) {
+		var store = new Store();
+		store.load();
+
 		var source = "http://impact.github.io/impact_index.json";
 
 		$.get(source, (result) => {
