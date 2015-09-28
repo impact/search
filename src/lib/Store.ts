ede7f1c8689ad5bdf126a43754f66edd64b7ea7d
Michael Tiller <michael.tiller@gmail.com>
2015-09-28 18:17:25 -0400

    Added some diagnostic messages and fallback index loading

--- src/lib/Store.ts
+++ src/lib/Store.ts
@@ -12,7 +12,9 @@ class Store {
 
 	constructor(public source?: string) {
 		if (!source) {
-			this.source = "/impact_index.json";
+			// Use the local version, if it exists, to avoid the need
+			// for a network connection.
+			this.source = "./impact_index.json";
 		}
 		this.index = new SubState(null);
 		this.term = new SubState("");
@@ -23,9 +25,47 @@ class Store {
 	}
 
 	load() {
-		return $.get(this.source, (result) => {
+		return $.get(this.source)
+			.then((result: ImpactIndex) => {
+				console.log("Loaded index from ", this.source);
+				this.index.update(result);
+			}, (e) => {
+				var remote = "http://impact.github.io/impact_index.json";
+
+				// If we get here, there was an error.  So now let's try
+				// a public version of impact_index.json (requires a
+				// network connection).
+				console.log("Failed to load index from ", this.source);
+				console.log("Now tring to load from ", remote);
+				return $.get(remote)
+					.then((result: ImpactIndex) => {
+						console.log("Loaded index from ", remote);
+						this.index.update(result);
+					}, (e) => {
+						console.error("Unable to load index file from ", this.source,
+									  " or ", remote);
+					})
+			});
+		/*
+		return $.get(this.source, (result: ImpactIndex) => {
+			console.log("Loaded index from ", this.source);
 			this.index.update(result);
+		}, (e) => {
+			var remote = "http://impact.github.io/impact_index.json";
+
+			// If we get here, there was an error.  So now let's try
+			// a public version of impact_index.json (requires a
+			// network connection).
+			console.log("Failed to load index from ", this.source);
+			console.log("Now tring to load from ", remote);
+			return $.get(remote, (result: ImpactIndex) => {
+				console.log("Loaded index from ", remote);
+				this.index.update(result);
+			}, (e) => {
+				console.error("Unable to load index file from ", this.source, " or ", remote);
+			})
 		})
+		*/
 	}
 }
 
