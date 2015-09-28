f56d120f5f134677bf33c02f90834ec64c167a71
Michael Tiller <michael.tiller@gmail.com>
2015-09-28 17:34:48 -0400

    Cleaned out junk

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ var path = require('path');
 module.exports = {
 	context: path.join(__dirname, "src"),
     entry: "./entry.ts",
-	//context: path.join(__dirname, "build"),
-    //entry: "./entry.js",
 	resolve: {
 		modulesDirectories: ["build", "node_modules", "src"],
 		extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
@@ -22,22 +20,3 @@ module.exports = {
         ]
     }
 };
-
-/*
-module.exports = {
-	context: path.join(__dirname, "src"),
-    entry: "./entry.ts",
-    output: {
-		path: path.join(__dirname, './dist'),
-		publicPath: "/",
-        filename: "./bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" },
-			{ test: /\.ts$/, loader: 'ts-loader' },
-			{ test: /\.tsx$/, loader: 'ts-loader' }
-        ]
-    }
-};
-*/
