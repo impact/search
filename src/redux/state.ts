29758d448bf1bd2bd52e268dd78dc1ea23757520
Michael Tiller <michael.tiller@gmail.com>
2015-11-05 18:11:16 -0500

    Added some tests for search term matching

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -1,33 +1,43 @@
-import { ImpactIndex } from '../impact/Index';
+import { ImpactIndex, Library } from '../impact/Index';
 import { handleActions, Action, Reducer } from 'redux-actions';
+import { computeResults } from '../impact/search';
 
 import * as actions from './actions';
 
 export class State {
 	public term: string;
 	public index: ImpactIndex;
+	public matching: Library[];
 
 	// The constructor creates a reasonable initial state
 	constructor() {
 		this.term="";
-		this.index = null;
+		this.index = {
+			version: "1.0.0",
+			libraries: []
+		};
+		this.matching = [];
 	}
 
 	public setTerm(term: string): State {
 		var ret = this.clone();
 		ret.term = term;
+		ret.matching = computeResults(term, this.index);
 		return ret;
 	}
 
 	public loadIndex(index: ImpactIndex): State {
 		var ret = this.clone();
 		ret.index = index;
+		ret.matching = computeResults(this.term, this.index);
 		return ret;
 	}
 
 	private clone(): State {
 		var ret = new State();
 		ret.term = this.term
+		ret.index = this.index
+		ret.matching = this.matching
 		return ret
 	}
 }
--- tests/test_state.ts
+++ tests/test_state.ts
@@ -12,13 +12,15 @@ describe("Create an initial state", () => {
 	it('should have an empty search term and no libraries', () => {
 		var state = new State()
 		expect(state.term).to.equal("")
-		expect(state.index).to.equal(null)
+		expect(state.index.libraries).to.deep.equal([])
+		expect(state.matching).to.deep.equal([])
 	})
 	it('should set the search term on a search term action', () => {
 		var store = createStore(rootReducer);
 		store.dispatch(actions.setTerm("buildings"));
 		expect(store.getState().term).to.equal("buildings")
-		expect(store.getState().index).to.equal(null)
+		expect(store.getState().index.libraries).to.deep.equal([])
+		expect(store.getState().matching).to.deep.equal([])
 	})
 })
 describe("Load libraries", () => {
@@ -26,24 +28,34 @@ describe("Load libraries", () => {
 		var store = createStore(rootReducer);
 		store.dispatch(actions.loadIndex(sample));
 		expect(store.getState().term).to.equal("")
-		expect(store.getState().index).to.not.equal(null)
+		expect(store.getState().index.libraries).to.not.deep.equal([])
 		expect(store.getState().index.libraries.length).to.not.equal(0)
 		expect(store.getState().index.libraries.length).to.equal(66)
+		expect(store.getState().matching.length).to.equal(0)
+
+		store.dispatch(actions.setTerm("*"));
+		expect(store.getState().index.libraries).to.not.deep.equal([])
+		expect(store.getState().index.libraries.length).to.not.equal(0)
+		expect(store.getState().index.libraries.length).to.equal(66)
+		expect(store.getState().matching.length).to.equal(66)
+
+		store.dispatch(actions.setTerm("building"));
+		expect(store.getState().index.libraries).to.not.deep.equal([])
+		expect(store.getState().index.libraries.length).to.not.equal(0)
+		expect(store.getState().index.libraries.length).to.equal(66)
+		expect(store.getState().matching.length).to.equal(3)
 	})
-	it('should load index from the network', (done) => {
+	it('should load index from the network', () => {
 		var store = createStore(rootReducer);
 
 		var lp = actions.load("http://impact.github.io/impact_index.json", store)
 
-		lp.then((index: ImpactIndex) => {
+		return lp.then((index: ImpactIndex) => {
 			console.log("Loaded!")
 			expect(store.getState().term).to.equal("")
-			expect(store.getState().index).to.not.equal(null)
+			expect(store.getState().index.libraries).to.not.deep.equal([])
 			expect(store.getState().index.libraries.length).to.not.equal(0)
-
-			done();
-		}, (e) => {
-			throw new Error(e);
+			expect(store.getState().matching.length).to.equal(0)
 		});
 	})
 })
