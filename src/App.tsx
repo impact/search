c24725c36c49d496d09d4b21b3329128501969b2
Michael Tiller <michael.tiller@xogeny.com>
2019-11-18 21:06:20 -0500

    Got front page kind of working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./impact-logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { FormGroup, InputGroup } from "@blueprintjs/core";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    const [terms, setTerms] = React.useState("");
+    return (
+        <Router>
+            <Switch>
+                <Route path="/">
+                    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                        <img style={{ width: 400 }} src={logo}></img>
+                        <p
+                            style={{
+                                fontSize: "200%",
+                                fontWeight: 300,
+                                fontFamily: `"Helvetica Neue",Helvetica,Arial,sans-serif`,
+                            }}
+                        >
+                            The Modelica Search Engine
+                        </p>
+                        <div>
+                            <InputGroup
+                                fill={false}
+                                autoFocus={true}
+                                style={{ maxWidth: "20em", width: "100%" }}
+                                leftIcon="search"
+                                onChange={(e: React.FormEvent<HTMLElement>) =>
+                                    setTerms((e.target as HTMLInputElement).value)
+                                }
+                                placeholder="Search terms..."
+                                value={terms}
+                            />
+                        </div>
+                    </div>
+                </Route>
+                <Route path="/libs">Libraries</Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
